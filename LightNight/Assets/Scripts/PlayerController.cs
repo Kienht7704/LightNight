5ce429e1e450a31795b95657296c7221e213a03f
Kienht7704 <kienht7704@gmail.com>
2026-01-17 20:48:34 +0700

    Drive update

--- LightNight/Assets/Scripts/PlayerController.cs
+++ LightNight/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
