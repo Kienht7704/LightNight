19a62dbf8a9b711f95b402e4b4e6f00e94cc75ba
Tad-Min <nguyenvanthanhdat44@gmail.com>
2026-03-09 23:51:04 +0700

    ui game

---
+++ LightNight/Assets/Scripts/TimerUI.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+using TMPro;
+
+public class TimerUI : MonoBehaviour
+{
+    public static TimerUI Instance;
+
+    [Header("Giao diện UI")]
+    public TextMeshProUGUI timerText; // Kéo TxtTimer vào đây
+
+    private float currentTime = 0f;
+    private bool isTimerRunning = false;
+
+    void Awake()
+    {
+        // Khởi tạo Singleton
+        if (Instance == null) Instance = this;
+    }
+
+    void Start()
+    {
+        // Vừa vào game thì hiển thị 00:00:00
+        UpdateTimerDisplay();
+    }
+
+    void Update()
+    {
+        // Chỉ cộng thời gian khi biến này bằng true
+        if (isTimerRunning)
+        {
+            currentTime += Time.deltaTime; // Thời gian thực trôi qua
+            UpdateTimerDisplay();
+        }
+    }
+
+    // --- CÁC HÀM CÔNG CỤ ĐỂ GỌI TỪ NƠI KHÁC ---
+
+    // 1. Lệnh bắt đầu đếm
+    public void StartTimer()
+    {
+        isTimerRunning = true;
+    }
+
+    // 2. Lệnh dừng đếm
+    public void StopTimer()
+    {
+        isTimerRunning = false;
+    }
+
+    // 3. Lệnh reset về 0
+    public void ResetTimer()
+    {
+        currentTime = 0f;
+        UpdateTimerDisplay();
+    }
+
+    // Hàm nội bộ để định dạng chữ số cho đẹp (Phút : Giây : MiliGiây)
+    private void UpdateTimerDisplay()
+    {
+        if (timerText == null) return;
+
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        int milliseconds = Mathf.FloorToInt((currentTime * 100) % 100); // Lấy 2 số thập phân
+
+        // Hiển thị định dạng MM:SS:ms (VD: 01:25:40)
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
+    }
+}
---
+++ LightNight/Assets/Scripts/UserManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.IO; // Để thao tác Đọc/Ghi File
+using System;    // Để dùng thư viện sinh mã UUID (Guid)
+
+
+[System.Serializable]
+public class UserData
+{
+    public string userId;
+    public string userName;
+}
+
+public class UserManager : MonoBehaviour
+{
+    [Header("Thông tin người chơi")]
+    public string currentUserId;
+    public string currentUserName;
+
+    private string saveFilePath;
+
+    void Awake()
+    {
+        saveFilePath = Application.persistentDataPath + "/UserData.json";
+
+        LoadOrGenerateUserId();
+    }
+
+    public void LoadOrGenerateUserId()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string jsonContent = File.ReadAllText(saveFilePath);
+
+            UserData data = JsonUtility.FromJson<UserData>(jsonContent);
+
+            if (data != null )
+            {
+                if(!string.IsNullOrEmpty(data.userId))
+                    currentUserId = data.userId;
+                if(!string.IsNullOrWhiteSpace(data.userName))
+                    currentUserName = data.userName;
+            }
+            else
+            {
+                GenerateAndSaveNewId();
+            }
+
+        }
+        else
+        {
+            GenerateAndSaveNewId();
+        }
+    }
+
+    public void SaveCurrentUserName()
+    {
+        UserData updatedData = new UserData();
+        updatedData.userId = currentUserId;
+        updatedData.userName = currentUserName;
+        string jsonToSave = JsonUtility.ToJson(updatedData, true);
+        File.WriteAllText(saveFilePath, jsonToSave);
+    }
+
+    private void GenerateAndSaveNewId()
+    {
+        currentUserId = Guid.NewGuid().ToString();
+        currentUserName = "UserName";
+        UserData newData = new UserData();
+        newData.userId = currentUserId;
+        newData.userName = currentUserName;
+
+        string jsonToSave = JsonUtility.ToJson(newData, true);
+        File.WriteAllText(saveFilePath, jsonToSave);
+    }
+}
