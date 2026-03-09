aa4556a2726694ddb2c229b35d698352b616603d
Tad-Min <nguyenvanthanhdat44@gmail.com>
2026-03-09 23:51:04 +0700

    ui game

---
+++ LightNight/Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+public class Record
+{
+    public string userId;
+    public string userName;
+    public Time endTime;
+}
+
+public class GameManager
+{
+    
+}
